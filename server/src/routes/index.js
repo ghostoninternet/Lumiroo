fd4a97ef6ccfe10984bbc6acd17eabbfad727302
ghostoninternet <daosyphuc288@gmail.com>
2024-11-24 14:51:59 +0700

    feature: API file upload

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 router.use('/auth', require('./auth'))
 router.use('/playgrounds', require('./playground'))
+router.use('/uploads', require('./upload'))
 
 module.exports = router
