fe7feea2fe961b831679cfd46ebea2b2e2d13605
GiaHuyJQK1706 <huydogiaccac@gmail.com>
2024-11-24 21:16:55 +0700

    Tạo api đăng nhập, đăng ký, kết nối đến CSDL

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -3,30 +3,37 @@ const userDaos = require('../daos/user.daos')
 const { NotFoundError, BadRequestError, UserAlreadyExistError } = require('../errors/customError')
 
 const signin = async ({ email, password }) => {
-  const foundUser = await userDaos.findUserByEmail(email)
-  if (!foundUser) throw new NotFoundError('User not found!')
+  // Tìm người dùng bằng email
+  const foundUser = await userDaos.findUserByEmail(email);
+  if (!foundUser) throw new NotFoundError('User not found!');
 
-  const passwordComapre = await bcrypt.compare(password, foundUser.password)
-  if (!passwordComapre) throw new BadRequestError('Password is incorrect')
-  
-  return foundUser
-}
+  // So sánh mật khẩu
+  const passwordCompare = await bcrypt.compare(password, foundUser.password);
+  if (!passwordCompare) throw new BadRequestError('Password is incorrect');
 
-const signup = async ({ email, password }) => {
-  const foundUser = await userDaos.findUserByEmail(email)
-  if (foundUser) throw new UserAlreadyExistError()
+  return foundUser; // Trả về thông tin người dùng
+};
 
-  const hashedPassword = await bcrypt.hash(password, 10)
-  
+const signup = async ({ email, password, gender, phoneNumber, dob, avatarUrl, role }) => {
+  const foundUser = await userDaos.findUserByEmail(email);
+  if (foundUser) throw new UserAlreadyExistError();
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const newUserData = {
     email,
     password: hashedPassword,
-  }
+    gender,
+    phoneNumber,
+    dob,
+    avatarUrl,
+    role,
+  };
 
-  const newUser = await userDaos.createNewUser(newUserData)
+  const newUser = await userDaos.createNewUser(newUserData);
 
-  return newUser
-}
+  return newUser;
+};
 
 const refreshToken = async () => {
 
@@ -39,6 +46,6 @@ const logout = async () => {
 module.exports = {
   signin,
   signup,
-  refreshToken,
-  logout,
+  refreshToken: async () => null,
+  logout: async () => null,
 }
