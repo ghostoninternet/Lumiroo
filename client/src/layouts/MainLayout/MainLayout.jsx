dabcbe8976124fb3da17f325e274e64a66fdc99f
Ha Duy Bach <111235354+HaDuyBachh@users.noreply.github.com>
2024-11-24 20:59:32 +0700

    change css to tailwind css (all)

--- client/src/layouts/MainLayout/MainLayout.jsx
+++ client/src/layouts/MainLayout/MainLayout.jsx
@@ -1,38 +1,24 @@
-import { Outlet } from 'react-router-dom'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import { Outlet } from 'react-router-dom';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 
 function MainLayout() {
   return (
-    <div style={styles.layout}>
+    <div className="flex flex-col min-h-screen">
       <header>
-        <Header role="user"/>
+        <Header role="user" />
       </header>
 
-      <main style={styles.mainContent}>
+      <main className="flex-grow p-5 bg-gray-100">
         <h2>This main layout of website!</h2>
         <p>This is the main content of the page.</p>
       </main>
 
-      <footer style={styles.footer}>
-        <Footer/>
+      <footer>
+        <Footer />
       </footer>
-
     </div>
   );
 }
 
-const styles = {
-  layout: {
-    display: 'flex',         
-    flexDirection: 'column',       
-    minHeight: '100vh',            
-  },
-  mainContent: {
-    flexGrow: 1,                  
-    padding: '20px',
-    backgroundColor: '#f4f4f4',
-  },
-};
-
-export default MainLayout
+export default MainLayout;
