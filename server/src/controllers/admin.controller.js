ffa427d36b401b5ca5f5be8580dadbecf5b865ed
ghostoninternet <daosyphuc288@gmail.com>
2024-12-21 14:59:35 +0700

    topic: Codebase for Admin APIs

---
+++ server/src/controllers/admin.controller.js
@@ -0,0 +1,57 @@
+const getManyUsers = async (req, res, next) => {
+
+}
+
+const getUserDetail = async (req, res, next) => {
+
+}
+
+const searchUsers = async (req, res, next) => {
+
+}
+
+const updateUser = async (req, res, next) => {
+
+}
+
+const deleteUser = async (req, res, next) => {
+
+}
+
+const getManyPlaygrounds = async (req, res, next) => {
+
+}
+
+const getPlaygroundDetail = async (req, res, next) => {
+
+}
+
+const createNewPlayground = async (req, res, next) => {
+
+}
+
+const updatePlayground = async (req, res, next) => {
+
+}
+
+const deletePlayground = async (req, res, next) => {
+
+}
+
+const getDashboardData = async (req, res, next) => {
+
+}
+
+module.exports = {
+  getManyUsers,
+  getUserDetail,
+  searchUsers,
+  updateUser,
+  deleteUser,
+  getManyPlaygrounds,
+  getPlaygroundDetail,
+  createNewPlayground,
+  updatePlayground,
+  deletePlayground,
+  getDashboardData
+}
